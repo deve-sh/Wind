4f35c345fb087790339c10280c796ad44ec738b2
Devesh Kumar <devesh2027@gmail.com>
2021-04-29 21:40:11 +0530

    Update read_file file

--- db/utils/read_file.py
+++ db/utils/read_file.py
@@ -1,8 +1,8 @@
 # Gets and reads file.
 
-def readfile(filename = ""):
+def read_file(filename = "", binary = False):
     try:
-        file = open(filename, "r")
+        file = open(filename, "r" if not binary else "rb")
         fileContents = file.read()
         file.close()
         return fileContents
