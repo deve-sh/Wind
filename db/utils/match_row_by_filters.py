daf5fbf14cce1dcd1b3d0e902e40d5f26d0a7787
Devesh Kumar <devesh2027@gmail.com>
2021-05-02 10:34:38 +0530

    Added reusable match row by filters function.

---
+++ db/utils/match_row_by_filters.py
@@ -0,0 +1,7 @@
+def match_row_by_filter(row = {}, filters = {}):
+    all_filters_match = True
+    for key in filters:
+        if(not key in row or row[key] != filters[key]):
+            all_filters_match = False
+            break
+    return all_filters_match
