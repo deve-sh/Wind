583779a43b7b675a6c05e567228ed3fa5d28c845
Devesh Kumar <devesh2027@gmail.com>
2021-05-04 17:29:34 +0530

    Added command line api to drop table.

---
+++ db/drop_table.py
@@ -0,0 +1,40 @@
+# Command Line API that creates a Database table.
+
+import sys
+import json
+
+from lib.drop_table import drop_table
+
+arguments = sys.argv
+
+res = {
+    "status": 200
+}
+
+try:
+    if(len(arguments) <= 1):
+        res["status"] = 400
+        res["message"] = "Invalid Number of Arguments"
+    else:
+        receivedJSON = json.loads(arguments[1])
+        if(
+            not receivedJSON or 
+            "table_name" not in receivedJSON or 
+            not receivedJSON["table_name"]
+        ):
+            res["status"] = 400
+            res["message"] = "Table name not provided."
+        else:
+            table_name = receivedJSON["table_name"]
+            dropped_table = drop_table(table_name)
+
+            if(not dropped_table):
+                raise Exception("Table could not be dropped.")
+            
+            res["status"] = 201
+            res["message"] = "Dropped table successfully."
+except Exception as e:
+    res["status"] = 500
+    res["message"] = str(e)
+
+print(json.dumps(res))
