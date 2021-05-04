987f45f16eee181cd5bed88a7e6dfac70d5d4f34
Devesh Kumar <devesh2027@gmail.com>
2021-05-04 17:28:18 +0530

    Updated drop_table error handling.

--- db/lib/drop_table.py
+++ db/lib/drop_table.py
@@ -6,7 +6,10 @@ from lib.table import get_table_file_name
 def drop_table(table_name):
     table_filename = get_table_file_name(table_name)
     if os.path.exists(table_filename):
-        os.remove(table_filename)
-        return True
+        try:
+            os.remove(table_filename)
+            return True
+        except Exception as e:
+            raise Exception("Table could not be deleted.")
     else:
         raise Exception("Table not found.")
