cb72a977ead61cb4ab8e0da7cb9709cb4ddd1644
Devesh Kumar <devesh2027@gmail.com>
2021-05-06 23:25:24 +0530

    Integrated nested field getter into filter matching util function.

--- db/lib/utils/match_row_by_filters.py
+++ db/lib/utils/match_row_by_filters.py
@@ -1,7 +1,14 @@
+from lib.utils.get_nested_field import get_nested_field
+
 def match_row_by_filters(row = {}, filters = {}):
     all_filters_match = True
     for key in filters:
-        if(not key in row or row[key] != filters[key]):
+        if("." in key):
+            # If the user is using dot notations to query inner object structures.
+            if(get_nested_field(row, key) != filters[key]):
+                all_filters_match = False
+                break
+        elif(not key in row or row[key] != filters[key]):
             all_filters_match = False
             break
     return all_filters_match
