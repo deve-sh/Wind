643256ad4c40dee7a65757881dc2853c1e7a0ec6
Devesh Kumar <devesh2027@gmail.com>
2021-05-09 10:05:56 +0530

    Added list of query types to support in the release.

--- db/lib/utils/match_row_by_filters.py
+++ db/lib/utils/match_row_by_filters.py
@@ -1,11 +1,15 @@
 from lib.utils.get_nested_field import get_nested_field
 
+reserved_querying_filters = ["_or"]
+reserved_membership_filters = ["_in", "_nin", "_includes"]
+
 def match_row_by_filters(row = {}, filters = {}):
     all_filters_match = True
     for key in filters:
         if("." in key):
             # If the user is using dot notations to query inner object structures.
-            if(get_nested_field(row, key) != filters[key]):
+            value_to_match = get_nested_field(row, key)
+            if(value_to_match != filters[key]):
                 all_filters_match = False
                 break
         elif(not key in row or row[key] != filters[key]):
