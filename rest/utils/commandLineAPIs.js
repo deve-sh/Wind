0970e0f43a4e2edbd591e73551a28c1fc7bbadea
Devesh Kumar <devesh2027@gmail.com>
2021-05-12 21:35:34 +0530

    Added constants for command line APIs.

---
+++ rest/utils/commandLineAPIs.js
@@ -0,0 +1,13 @@
+const commandLineAPIs = {
+    QUERY_TABLE: "query_table.py",
+    COUNT_ROWS: "count_rows.py",
+    DROP_TABLE: "drop_table.py",
+    CREATE_TABLE: "create_table.py",
+    INSERT_ROW: "insert_row.py",
+    SUM_FIELDS: "sum_fields.py",
+    MAX_OF_FIELDS: "max_of_fields.py",
+    MIN_OF_FIELDS: "min_of_fields.py",
+    AVG_OF_FIELDS: "avg_of_fields.py",
+};
+
+module.exports = commandLineAPIs;
