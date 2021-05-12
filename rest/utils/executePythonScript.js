df5fd20b4344426d4c7581e14f167701bf4327d8
Devesh Kumar <devesh2027@gmail.com>
2021-05-12 21:25:50 +0530

    Added error handling and long data acceptance from child process.

--- rest/utils/executePythonScript.js
+++ rest/utils/executePythonScript.js
@@ -3,15 +3,27 @@ const { spawn } = require("child_process");
 const executePythonScript = async (scriptName = "", arguments = []) => {
     return new Promise((resolve, reject) => {
         if(!scriptName) return reject("Script name invalid.");
-        let dataToSend;
+
+        let dataToSend = [];
+        let errorToSend = "";
+
+        let hasEncounteredError = false;
 
         const python = spawn('python', ['scriptName', ...arguments]);
 
-        python.stdout.on('data', function (data) {
-            dataToSend = data.toString();
+        python.stdout.on('data', (data) => {
+            dataToSend.push(data.toString());
         });
 
-        python.on('close', () => resolve(dataToSend));
+        python.on('error', (err) => {
+            hasEncounteredError = true;
+            errorToSend = err;
+        });
+
+        python.on('close', () => {
+            if(hasEncounteredError) reject(errorToSend);
+            resolve(dataToSend.join(""))
+        });
     });
 }
 
