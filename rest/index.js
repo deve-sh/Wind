fcbdf759610344eb7323fc5a3f1592869b0c4919
Devesh Kumar <devesh2027@gmail.com>
2021-06-01 22:27:30 +0530

    Added basic health check route & 404 route.

--- rest/index.js
+++ rest/index.js
@@ -7,4 +7,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
+app.get("/", (_, res) =>
+	res.status(200).json({
+		message: "Health Check.",
+	})
+);
+
+app.all("*", (_, res) => res.sendStatus(404));
+
 app.listen(process.env.PORT || 8900);
